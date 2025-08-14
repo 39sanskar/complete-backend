cdc290ca8b3a1f93840a1515687e239bc3cde9a3
39sanskar <mishrasanskar496@gmail.com>
2025-08-14 05:51:03 +0530

    cat-image generate

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const catMe = require('cat-me');
+
+console.log(catMe());
+
+/*
+
+My favourite cat 
+
+ /\     /\
+{  `---'  }
+{  O   O  }
+~~>  V  <~~
+ \  \|/  /
+  `-----'__
+  /     \  `^\_
+ {       }\ |\_\_   W
+ |  \_/  |/ /  \_\_( )
+  \__/  /(_E     \__/
+    (  /
+     MM
+     
+*/
