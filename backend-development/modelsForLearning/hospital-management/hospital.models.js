58aa9ca128f74bee4f9816222af08c3f4c8e4632
39sanskar <mishrasanskar496@gmail.com>
2025-08-18 03:29:07 +0530

    hospitalSchema

---
+++ backend-development/modelsForLearning/hospital-management/hospital.models.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose'
+
+const hospitalSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  addressLine1: {
+    type: String,
+    required: true,
+  },
+  addressLine2: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  pincode: { // Remember: Internationalisation (come country has pincode numbers and alphabets. So type is String)
+    type: String,
+    required: true
+  },
+  specializedIn: [
+    {
+      type: String,
+      required: true
+    }
+  ],
+  phoneNumber: {
+    type: String,
+    required: true,
+    match: [/^\+?[0-9]{7,15}$/, "Please enter a valid phone number"]
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"] 
+  },
+  totalBeds:{
+    type: Number,
+    default: 0,
+    required: true
+  },
+  availableBeds: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  facitities: [
+    {
+    type: String,
+    enum: ["ICU", "Emergency", "Pharmacy", "Lab", "Blood Bank", "Radiology"]
+    }
+  ],
+  averageRating: {
+    type: Number,
+    default: 0,
+    min: 0,
+    max: 5
+  },
+  numberOfReviews: {
+    type: Number,
+    default: 0
+  }
+}, {timestamps: true});
+
+export const Hospital = mongoose.model('HospitaL', hospitalSchema);
