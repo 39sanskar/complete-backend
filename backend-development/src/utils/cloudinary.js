3c95dd736ba91736a8ef5e71f0f135c663db1c07
39sanskar <mishrasanskar496@gmail.com>
2025-08-21 19:16:08 +0530

    logic to upload files on cloudinary

---
+++ backend-development/src/utils/cloudinary.js
@@ -0,0 +1,39 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+
+        fs.unlinkSync(localFilePath); // remove file after upload
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+};
+
+// add this
+const deleteFromCloudinary = async (publicId, resourceType = "image") => {
+    try {
+        const result = await cloudinary.uploader.destroy(publicId, {
+            resource_type: resourceType,
+        });
+        return result;
+    } catch (error) {
+        console.error("Error deleting from Cloudinary:", error);
+        return null;
+    }
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary };
