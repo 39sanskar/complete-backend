4a68033e759fd648aef0aa6e50492e7f09f13c0c
39sanskar <mishrasanskar496@gmail.com>
2025-08-27 22:09:23 +0530

    basic-routes

---
+++ bun(Node.js Alternative)/bun-first/index.ts
@@ -0,0 +1,41 @@
+import figlet from "figlet";
+
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(req){
+    const url = new URL(req.url)
+
+    if(url.pathname === '/'){
+      const body = figlet.textSync("I am learning bun")
+    return new Response(body)
+    }
+    if(url.pathname === "/about"){
+      return new Response("About me")
+    }
+
+    if(url.pathname === "/contact"){
+      return new Response("Contact Us!")
+    }
+    
+    // handle Error
+    if(url.pathname === "/feed"){
+      throw new Error("Could not fetch feed")
+    }
+
+    if(url.pathname === '/greet'){
+      return new Response(Bun.file('./greet.txt'))
+    }
+    
+    return new Response("Not Found", { status: 404 })
+  },
+  error(error){
+    return new Response(`<pre> ${error} \n ${error.stack} </pre>`,{
+      headers: {
+        'Content-Type': 'text/html'
+      }
+    })
+  }
+})
+
+console.log(`Listening on PORT http://localhost:${server.port}`)
